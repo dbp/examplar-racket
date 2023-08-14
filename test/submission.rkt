064e399278fa3ea027c638fa1cd208aa8a18ff08
Daniel Patterson <dbp@dbpmail.net>
2023-08-13 21:27:39 -0400

    don't use htdp checked signatures in test files! Means can only use 8.9 or above...

--- test/submission.rkt
+++ test/submission.rkt
@@ -1,6 +1,6 @@
 #lang htdp/bsl
 
-(: F (Natural -> Natural))
+;(: F (Natural -> Natural))
 ; Computes the factorial of a number
 (check-expect (F 0) 1)
 (check-expect (F 1) 1)
@@ -11,7 +11,7 @@
   (cond [(= n 0) 1]
         [else (* n (F (- n 1)))]))
 
-(: I ([ListOf String] -> String))
+;(: I ([ListOf String] -> String))
 ; Joins a list of strings, separating with spaces
 (check-expect (I (list "a" "b" "c")) "a b c ")
 (check-expect (I (list "a")) "a ")
